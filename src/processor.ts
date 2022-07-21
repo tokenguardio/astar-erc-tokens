035b3646f74cfbf7a6f70e487e3535026c713df3
mckrava <mckrava2@gmail.com>
2022-07-21 18:28:37 +0300

    fix: add additional check for transfer handling

--- src/processor.ts
+++ src/processor.ts
@@ -46,7 +46,11 @@ processor.run(database, async (ctx: Context) => {
           try {
             await modules.handleErc20Transfer(ctx, block.header, item.event);
           } catch (e) {
-            await modules.handleErc721Transfer(ctx, block.header, item.event);
+            try {
+              await modules.handleErc721Transfer(ctx, block.header, item.event);
+            } catch (error) {
+              console.log(error);
+            }
           }
         }
       }
