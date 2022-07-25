10e49e6f20e35bfc8c39dd1063239724be8584ea
mckrava <mckrava2@gmail.com>
2022-07-25 10:13:57 +0300

    fix: unpdate timeout config

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,6 @@ export const batchSize = process.env.BATCH_SIZE
   ? parseInt(process.env.BATCH_SIZE)
   : 60;
 
-export const contractCallTimeout = process.env.CONTRACT_CALL_TIMEOUT
-  ? parseInt(process.env.CONTRACT_CALL_TIMEOUT)
-  : 20;
+export const contractCallTimeout = 20;
 
 export const chainNode = process.env.CHAIN_NODE || 'wss://rpc.astar.network';
