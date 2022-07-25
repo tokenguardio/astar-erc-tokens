942ae75d6e3179d8e8c22fc96bfcdeefc26f98cf
mckrava <mckrava2@gmail.com>
2022-07-25 10:17:27 +0300

    fix: reduce timeout config

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ export const batchSize = process.env.BATCH_SIZE
   ? parseInt(process.env.BATCH_SIZE)
   : 60;
 
-export const contractCallTimeout = 20;
+export const contractCallTimeout = 5;
 
 export const chainNode = process.env.CHAIN_NODE || 'wss://rpc.astar.network';
