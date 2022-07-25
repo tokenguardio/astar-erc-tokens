e53c913f6147cf08fadd5c77785659d42a23bbb3
mckrava <mckrava2@gmail.com>
2022-07-25 10:23:49 +0300

    fix: reducs batch size

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 export const batchSize = process.env.BATCH_SIZE
   ? parseInt(process.env.BATCH_SIZE)
-  : 60;
+  : 30;
 
-export const contractCallTimeout = 3;
+export const contractCallTimeout = 5;
 
 export const chainNode = process.env.CHAIN_NODE || 'wss://rpc.astar.network';
